f4f7f3d6558396e76bdd6061c51ed463910b52ce
Kobi Donner <kobibo@gmail.com>
2020-06-10 20:19:40 +0300

    Added simple gallery, click on an image updates gMeme and canvas

--- js/controller-meme-gen.js
+++ js/controller-meme-gen.js
@@ -2,7 +2,6 @@
 console.log('memegen-controller')
 
 function onInit() {
-    // debugger;
     resizeCanvas();
     drawMemeImg();
     renderMemeText();
@@ -11,23 +10,19 @@ function onInit() {
 function renderMemeText() {
     var memeText = getText();
     var elTextInput = document.getElementById('meme-text')
-    // console.log(elTextInput)
     elTextInput.value = memeText;
 }
 
 function onTxtEdit(el) {
-    // console.log('onTxtEdit')
     var newText = el.value
-    // console.log(newText)
     changeText(newText);
     drawMemeImg();
     renderMemeText()
 }
 
 function onClickGallery(el) {
-    console.log('onClickGallery', el)
-    var elSelectedImage = el.value;
-    console.log(elSelectedImage)
-    // var newImgId = el.value;
-    // console.log(newImgId);
+    var newImgId = el.getAttribute('value');
+    changeImg(newImgId);
+    drawMemeImg();
+    renderMemeText();
 }
