db66102b61480deb6a8553ebe53c2d8256b74d61
Kobi Donner <kobibo@gmail.com>
2020-06-10 16:41:59 +0300

    Created a canvas with a single image, taken from gMeme, manage by service_meme-gen

--- js/controller-meme-gen.js
+++ js/controller-meme-gen.js
@@ -1,3 +1,7 @@
 'use strict';
 console.log('memegen-controller')
-console.log('hi')
+
+
+function onInit() {
+    drawMemeImg();
+}
--- js/service_meme-gen.js
+++ js/service_meme-gen.js
@@ -1,2 +1,50 @@
 'use strict';
 console.log('memegen-service')
+
+const gElCanvas = document.getElementById('meme-canvas');
+var gCtx;
+
+var gKeywords = {
+    'happy': 12,
+    'funny': 4,
+    'ironic': 32,
+    'amazed': 8,
+}
+
+var gImgs = [
+    { id: 1, url: 'img/1.jpg', keywords: ['angry', 'announce'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['cute'] },
+]
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineCount: 1,
+    lines: [
+        {
+            txt: 'One shall never eat Falafel',
+            size: 20,
+            align: 'center',
+            color: 'white'
+        }
+    ]
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function getImageById(id) {
+    let idx = id - 1;
+    return gImgs[idx]
+}
+
+function drawMemeImg() {
+    gCtx = gElCanvas.getContext('2d');
+    var meme = getMeme();
+    var id = meme.selectedImgId;
+    var elImg = new Image();
+    elImg.src = getImageById(id).url;
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    }
+}
